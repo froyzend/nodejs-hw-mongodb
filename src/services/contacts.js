d1676f92284f67d58201b1811e617b60a613e8e1
Froyzend <froyzend1993@gmail.com>
2025-01-10 19:57:22 +0100

    First commit

---
+++ src/services/contacts.js
@@ -0,0 +1,12 @@
+// src/services/students.js
+import { ContactsCollection } from '../db/Models/contacts.js';
+
+export const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+  return contacts;
+};
+
+export const getContactById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
+};
