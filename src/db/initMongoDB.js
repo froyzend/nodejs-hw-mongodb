33005d0da1da8e9ef85a712748beb2bae4c25cc3
Froyzend <froyzend1993@gmail.com>
2025-01-13 09:29:33 +0100

    add init connction string

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -10,7 +10,8 @@ export const initMongoConnection = async () => {
     const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0
+`,
     );
     console.log('Mongo connection successfully established!');
   } catch (e) {
