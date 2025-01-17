37a5172908b070e2a93615b4ae4236742d55b911
Froyzend <froyzend1993@gmail.com>
2025-01-17 12:55:12 +0100

    add all  changes

---
+++ src/controllers/contacts.js
@@ -0,0 +1,97 @@
+import createHttpError from 'http-errors';
+import {
+  getContactById,
+  getAllContacts,
+  createContact,
+  deleteContact,
+  updateContact,
+} from '../services/contacts.js';
+
+export const getAllContactsController = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status({
+      status: 200,
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getContactByIdController = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      throw createHttpError(404, 'Student not found');
+    }
+    res.status({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContactsController = async (req, res, next) => {
+  try {
+    const contact = await createContact(req.body);
+    res.status(201).json({
+      status: 201,
+      message: `Successfully created a contact!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.status(200).json();
+};
+
+export const upsertContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a contact!`,
+    data: result.contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
+  });
+};
---
+++ src/router/contacts.js
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import {
+  getContactByIdController,
+  getAllContactsController,
+  createContactsController,
+  deleteContactController,
+  upsertContactController,
+  patchContactController,
+} from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const router = Router();
+
+router.get('/contacts', ctrlWrapper(getAllContactsController));
+
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactsController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+
+router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
+
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+
+export default router;
