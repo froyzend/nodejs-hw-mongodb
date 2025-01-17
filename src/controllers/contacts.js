daa2a99a7c7730b20d1c53b70711ed81bf796fd6
Froyzend <froyzend1993@gmail.com>
2025-01-17 13:27:11 +0100

    change getcontactid

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -25,9 +25,9 @@ export const getContactByIdController = async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
     if (!contact) {
-      throw createHttpError(404, 'Student not found');
+      throw createHttpError(404, 'Contact not found');
     }
-    res.status({
+    res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
