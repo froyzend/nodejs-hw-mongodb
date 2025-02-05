f552eec2a13d87b9dd51a4eba762152d1f120699
Froyzend <froyzend1993@gmail.com>
2025-02-05 08:04:27 +0100

    change path

--- src/router/contacts.js
+++ src/router/contacts.js
@@ -9,37 +9,18 @@ import {
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { createContactSchema } from '../validation/contacts.js';
 import { updateContactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 router.use(authenticate);
-router.get(
-  '/',
-  validateBody(createContactSchema),
-  ctrlWrapper(getAllContactsController),
-);
+router.get('/', ctrlWrapper(getAllContactsController));
 
-router.get(
-  '/:contactId',
-  isValidId,
-  validateBody(createContactSchema),
-  ctrlWrapper(getContactByIdController),
-);
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post(
-  '/register',
-  validateBody(createContactSchema),
-  ctrlWrapper(createContactsController),
-);
-router.delete(
-  '/:contactId',
-  isValidId,
-  validateBody(createContactSchema),
-  ctrlWrapper(deleteContactController),
-);
+router.post('/contacts', ctrlWrapper(createContactsController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 router.put(
   '/:contactId',
